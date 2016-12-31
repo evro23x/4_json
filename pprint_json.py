b2d0ab0d50f83297c614504e2c53aa5ee2f7a514
evro23x <evro23x@mail.ru>
2016-12-31 07:49:35 +0300

    pretty print json format

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,15 @@
 import json
+import sys
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, "r") as shops:
+        return json.load(shops)
 
 
-def pretty_print_json(data):
-    pass
+def pretty_print_json(shops):
+    print(json.dumps(shops, indent=4, sort_keys=True, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    pass
+    pretty_print_json(load_data(sys.argv[1]))
